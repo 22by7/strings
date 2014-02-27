bdfbc1efd0feb9bdfa3f419a42617da68e4d147b
Ashutosh Dave <ashutosh_dave2000@yahoo.com>
2014-02-28 00:05:06 +0530

    Added jQuery scripts for custom messages

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$('.btn').click(function(){
+
+		$(".error").hide();
+		$(".success").hide();
+		var hasError = false;
+		var emailRegx = /((([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?/;
+		var isSubmitted = false;
+
+		var emailVal = $("#email").val();
+		if(emailVal == ''){
+			$("#signup").after('<span class="error">Please enter your email address.</span>');
+			hasError = true;
+		}
+
+		else if (!emailRegx.test(emailVal)) {
+			$("#signup").after('<span class="error">Enter a valid email address.</span>');
+			hasError = true;
+		};
+
+		if (hasError == true) {return false};
+
+		if (isSubmitted == false) {
+			$("#signup").after('<span class="success">Okay! We\'ll send you an invite when we\'re ready.</span>');
+			submitted = true;
+		};
+
+		if(isSubmitted == true) {return false};
+
+	});
+	
+	
+});
